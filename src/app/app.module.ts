92919239470aaedd7e71a03abd38832c051875fb
Paul Chesa <paulchesa1@gmail.com>
2020-11-10 14:58:37 +0300

    Project set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+
+const MAT_MODULES = [
+  MatSidenavModule,
+  MatListModule,
+  MatIconModule,
+  MatToolbarModule
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProfileComponent,
+    LogInComponent,
+    SignUpComponent
   ],
-  imports: [
+  imports: [    
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ...MAT_MODULES,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
