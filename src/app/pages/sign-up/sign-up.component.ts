2b34fa143cda81d3241e94fa868198a7ea0e350c
Paul Chesa <paulchesa1@gmail.com>
2020-11-11 16:42:39 +0300

    design of sign up form

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms"
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  
+  signUpForm = this.fb.group({
+    firstName:["", Validators.required],
+    secondName:["", Validators.required],
+    password:["", Validators.required],
+    confirmPassword:["",[Validators.required]],
+    email:["",[Validators.required, Validators.email]]
+  })
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  signUp():void{
+    console.log(this.signUpForm.value);
   }
 
 }
