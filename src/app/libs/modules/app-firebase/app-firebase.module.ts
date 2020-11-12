dafe1048bdbe8a99a24c99db81ebd0a4d5d6a8e8
Paul Chesa <paulchesa1@gmail.com>
2020-11-12 13:23:25 +0300

    update packages

--- src/app/libs/modules/app-firebase/app-firebase.module.ts
+++ src/app/libs/modules/app-firebase/app-firebase.module.ts
@@ -8,7 +8,7 @@ import { environment } from '../../../../environments/environment';
 
 @NgModule({
   declarations: [],
-  imports: [ CommonModule,AngularFireModule.initializeApp(environment.firebaseConfig)],
+  imports: [CommonModule, AngularFireModule.initializeApp(environment.firebaseConfig)],
   exports: [AngularFireModule, AngularFireAuthModule],
 })
 export class AppFirebaseModule { }
--- src/app/libs/services/auth.service.ts
+++ src/app/libs/services/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { BehaviorSubject, from, Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private user: BehaviorSubject<
+    Observable<firebase.User>
+  > = new BehaviorSubject<Observable<firebase.User>>(null);
+  user$ = this.user   
+    .asObservable()
+    .pipe(switchMap((user: Observable<firebase.User>) => user));
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    this.user.next(this.afAuth.authState);
+  }     
+
+  loginViaGoogle(): Observable<auth.UserCredential> {
+    return from(this.afAuth.signInWithPopup(new auth.GoogleAuthProvider()));
+  }
+
+  logout(): Observable<void> {
+    return from(this.afAuth.signOut());
+  }
 }
