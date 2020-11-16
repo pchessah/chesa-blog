bcec264aae33fad490818bd134ef2946d0a4ac73
Paul Chesa <paulchesa1@gmail.com>
2020-11-16 16:13:13 +0300

    Added bootstrap, started profile page, added guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LogInComponent } from './pages/log-in/log-in.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(["home"]);
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "log-in", component: LogInComponent },
   { path: "sign-up", component: SignUpComponent },
-  { path: "profile", component: ProfileComponent },
+  {
+    path: "profile", component: ProfileComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }
+  },
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "**", component: NotFoundComponent }
 ];
